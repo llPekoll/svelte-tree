e4052c92d4679170fc7faa7668d7738caf05ae5e
peko <yoyo.mepa@gmail.com>
2021-02-10 05:54:48 +0100

    better position for mesh

--- src/App.svelte
+++ src/App.svelte
@@ -30,17 +30,43 @@
     position: absolute; 
     color:grey;
     left: 15%; 
-    top: 50%; 
+    top: 20%; 
     width:200px; 
     height:100px; 
   }
-  
+  .projects{
+    position: absolute; 
+    left: 15%; 
+    top: 30%; 
+  }
+  .box{
+    background-color: red;
+    height: 10px;
+    width: 10px;
+  }
+  ol{
+    list-style-type: none;
+  }
 </style>
 <div id="wrapper">
     <div bind:this={title} class="title">
         {@html title_content}
     </div>
     
+</div>
+<!-- https://jsfiddle.net/elvinmammadoff/bqrLkdsc/ -->
+<div class="projects">
+  <ol>
+    <li>
+    <div class="box">
+    </div>
+      Tipsmeee
+    </li>
+    <li>Rolex</li>
+    <li>VR/AR Augment</li>
+    <li>VR/AR Light-up</li>
+    <li>Animation</li>
+  </ol>
 </div>
 
 <canvas bind:this={el}></canvas>
