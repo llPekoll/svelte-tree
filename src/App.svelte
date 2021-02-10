dfec7e150941ca4b886208dc479f65a5fc927809
peko <yoyo.mepa@gmail.com>
2021-02-10 05:48:23 +0100

    better position for mesh

--- src/App.svelte
+++ src/App.svelte
@@ -2,23 +2,23 @@
   import { onMount } from 'svelte';
   import { createScene } from "./scene";
   import {gsap} from 'gsap';
-  // import { SplitText } from "gsap/SplitText";
+  import { Swipe, SwipeItem } from "svelte-swipe";
+
+
   let title;
-  const title_content = "Yohann";
+  const title_content = "|Yohann <br/> |Mepa";
   
   let el;
   onMount(() => {
     createScene(el)
-    // gsap.registerPlugin(SplitText);
-    // var tl = gsap.timeline(), 
-
-    // mySplitText = new SplitText(title, {type:"words,chars"}), 
-    // chars = mySplitText.chars; //an array of all the divs that wrap each character
-
-    // gsap.set(title, {perspective: 400});
-
-    // tl.from(chars, {duration: 0.8, opacity:0, scale:0, y:80, rotationX:180, transformOrigin:"0% 50% -50",  ease:"back", stagger: 0.01}, "+=0");
   });
+    const swipeConfig = {
+    autoplay: false,
+    delay: 2000,
+    showIndicators: true,
+    transitionDuration: 1000,
+    defaultIndex: 0,
+  };
 </script>
 
 <style>
@@ -26,7 +26,7 @@
     font-family: 'Roboto';
     font-size: 44px;
     font-weight: 100;
-    line-height: 0.7;
+    line-height: 0.8;
     position: absolute; 
     color:grey;
     left: 15%; 
@@ -34,6 +34,7 @@
     width:200px; 
     height:100px; 
   }
+  
 </style>
 <div id="wrapper">
     <div bind:this={title} class="title">
